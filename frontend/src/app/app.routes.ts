8f2694c7948c4ae56919a2ad59179d8d09696823
eng-mohamed-I <mohamedyasiiin1@gmail.com>
2024-08-22 19:30:46 +0300

    add login add signup

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './Components/main-layout/main-layout.component';
 import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './Components/login/login.component';
 
 export const routes: Routes = [
     {
@@ -9,5 +10,8 @@ export const routes: Routes = [
         children:[
             {path: '', component: HomeComponent}
         ]
+    },
+    { 
+        path: "login", component: LoginComponent 
     }
 ];

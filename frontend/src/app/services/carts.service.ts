13946e9209a4f6a4c65ad6471b8fdd565d40fcaf
ahmed hassan <ahmedhsn787@gmail.com>
2024-08-24 10:59:53 +0300

    total Price & delete from Cart

--- frontend/src/app/services/carts.service.ts
+++ frontend/src/app/services/carts.service.ts
@@ -6,9 +6,12 @@ import { IProduct } from '../models/Iproduct';
 })
 export class CartsService {
   private cartItems: IProduct[] = [];
-  private cartProductsSource = new BehaviorSubject<{ product: IProduct; quantity: number }[]>([]);
+  private cartProductsSource = new BehaviorSubject<
+    { product: IProduct; quantity: number }[]
+  >([]);
   cartProducts = this.cartProductsSource.asObservable();
-  private cartItemCountSubject: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  private cartItemCountSubject: BehaviorSubject<number> =
+    new BehaviorSubject<number>(0);
 
   constructor() {
     this.loadCartItems();
@@ -26,7 +29,9 @@ export class CartsService {
 
   addProductToCart(product: IProduct): void {
     const currentProducts = this.cartProductsSource.value;
-    const existingProduct = currentProducts.find(item => item.product.id === product.id);
+    const existingProduct = currentProducts.find(
+      (item) => item.product.id === product.id
+    );
 
     if (existingProduct) {
       existingProduct.quantity += 1;
@@ -39,8 +44,20 @@ export class CartsService {
     this.saveCartItems();
   }
 
+  removeProductFromCart(productId: number): void {
+    const currentProducts = this.cartProductsSource.value.filter(
+      (item) => item.product.id !== productId
+    );
+    this.cartProductsSource.next(currentProducts);
+    this.updateCartItemCount();
+    this.saveCartItems();
+  }
+
   private updateCartItemCount(): void {
-    const count = this.cartProductsSource.value.reduce((acc, item) => acc + item.quantity, 0);
+    const count = this.cartProductsSource.value.reduce(
+      (acc, item) => acc + item.quantity,
+      0
+    );
     this.cartItemCountSubject.next(count);
   }
 
--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -4,7 +4,7 @@ import { forkJoin, map, Observable } from 'rxjs';
 import { IProduct } from '../models/Iproduct';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
   private baseUrl = 'https://fakestoreapi.com/products';
@@ -15,22 +15,19 @@ export class ProductService {
     return this.http.get<IProduct[]>(`${this.baseUrl}`);
   }
 
- 
-
   getProductsByCategory(category: string): Observable<any[]> {
     return this.http.get<any[]>(`${this.baseUrl}/category/${category}`);
   }
 
-getCombinedClothingProducts(): Observable<any[]> {
-  const menClothing$ = this.getProductsByCategory('men\'s clothing');
-  const womenClothing$ = this.getProductsByCategory('women\'s clothing');
+  getCombinedClothingProducts(): Observable<any[]> {
+    const menClothing$ = this.getProductsByCategory("men's clothing");
+    const womenClothing$ = this.getProductsByCategory("women's clothing");
 
-  return forkJoin([menClothing$, womenClothing$]).pipe(
-    map(([menClothing, womenClothing]) => [...menClothing, ...womenClothing])
-  );
-}
-getProductById(id: number): Observable<IProduct> {
-  return this.http.get<IProduct>(`${this.baseUrl}/${id}`);
-}
-
+    return forkJoin([menClothing$, womenClothing$]).pipe(
+      map(([menClothing, womenClothing]) => [...menClothing, ...womenClothing])
+    );
+  }
+  getProductById(id: number): Observable<IProduct> {
+    return this.http.get<IProduct>(`${this.baseUrl}/${id}`);
+  }
 }

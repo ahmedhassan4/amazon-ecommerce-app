81b2bace8a3c0ba2ba5724fccb2acb775269674f
ahmed hassan <ahmedhsn787@gmail.com>
2024-08-24 11:49:18 +0300

    add main-nav

--- frontend/src/app/Components/main-layout/main-layout.component.ts
+++ frontend/src/app/Components/main-layout/main-layout.component.ts
@@ -3,17 +3,21 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 import { ToastModule } from 'primeng/toast';
-import { MessageService } from 'primeng/api'; 
+import { MessageService } from 'primeng/api';
+import { NavMainComponent } from '../nav-main/nav-main.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent,RouterOutlet,ToastModule],
+  imports: [
+    NavbarComponent,
+    FooterComponent,
+    RouterOutlet,
+    ToastModule,
+    NavMainComponent,
+  ],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css',
-  providers: [MessageService] 
-
+  providers: [MessageService],
 })
-export class MainLayoutComponent {
-
-}
+export class MainLayoutComponent {}

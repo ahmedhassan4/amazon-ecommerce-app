8f9095fb43660e77c15a754733b6d34ed6e10221
andf.750@gmail.com <andf.750@gmail.com>
2024-08-24 22:22:23 +0300

    complete checkout process

--- frontend/src/app/Components/cart/cart.component.ts
+++ frontend/src/app/Components/cart/cart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartsService } from '../../services/carts.service';
 import { IProduct } from '../../models/Iproduct';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -14,8 +15,13 @@ export class CartComponent implements OnInit {
   cartProducts: { product: IProduct; quantity: number }[] = [];
   totalPrice: number = 0;
 
-  constructor(private cartService: CartsService) {}
-
+  constructor(
+    private cartService: CartsService,
+    private router: Router
+  ) {}
+  navigate(): void {
+    this.router.navigate(['/checkout']); 
+  }
   ngOnInit(): void {
     this.cartService.cartProducts.subscribe((products) => {
       this.cartProducts = products;

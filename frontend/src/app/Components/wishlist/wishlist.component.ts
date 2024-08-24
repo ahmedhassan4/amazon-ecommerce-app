0ebb77b72d4afd9b254c184404fccf177e777565
eng-mohamed-I <mohamedyasiiin1@gmail.com>
2024-08-24 16:23:24 +0300

    handle wishlist add product to cart, remove from wishlist , add wishlist in navbar,

--- frontend/src/app/Components/wishlist/wishlist.component.ts
+++ frontend/src/app/Components/wishlist/wishlist.component.ts
@@ -1,40 +1,47 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
+import { RouterModule } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { CartsService } from '../../services/carts.service';
 
 @Component({
   selector: 'app-wishlist',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './wishlist.component.html',
   styleUrl: './wishlist.component.css'
 })
 export class WishlistComponent implements OnInit  {
   isLoggedIn!:boolean
-  constructor(private authServ : AuthService) { }
+  constructor(private authServ : AuthService,private cartServive : CartsService) { }
   ngOnInit(): void {
   this.authServ.getUserState().subscribe(state => this.isLoggedIn = state)
 }
   wishlist = [
     {
-      id: 1,
-      name: 'Product Name 1',
-      description: 'Short product description goes here.',
-      price: 29.99,
-      image: 'https://m.media-amazon.com/images/I/51rF5TKzVTL._AC_SX522_.jpg'
+      "id": 1,
+      "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+      "price": 109.95,
+      "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+      "category": "men's clothing",
+      "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      "rating": {
+      "rate": 3.9,
+      "count": 120
+      },
     },
     {
-      id: 2,
-      name: 'Product Name 2',
-      description: 'Short product description goes here.',
-      price: 49.99,
-      image: 'https://m.media-amazon.com/images/I/817QL8-1+GL._AC_SX679_.jpg'
-    },  {
-      id: 3,
-      name: 'Product Name 3',
-      description: 'Short product description goes here.',
-      price: 29.99,
-      image: 'https://m.media-amazon.com/images/I/616for3q0ML._AC_SX679_.jpg'
+      "id": 2,
+      "title": "Mens Casual Premium Slim Fit T-Shirts ",
+      "price": 22.3,
+      "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+      "category": "men's clothing",
+      "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+      "rating": {
+      "rate": 4.1,
+      "count": 259
+      }
     }
     // Add more items here...
   ];
@@ -45,6 +52,6 @@ export class WishlistComponent implements OnInit  {
 
   moveToCart(item: any) {
     console.log('Move to cart:', item);
-    this.removeFromWishlist(item.id);
+    this.cartServive.addProductToCart(item)
   }
 }

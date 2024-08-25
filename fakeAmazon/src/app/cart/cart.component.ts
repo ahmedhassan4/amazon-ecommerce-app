0229d77bde068d0704934298e170eda17d9f1152
NadaAtef255 <atefn918@gmail.com>
2024-08-25 15:15:46 +0300

    checking cart validation

---
+++ fakeAmazon/src/app/cart/cart.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CartsService } from '../../services/carts.service';
+import { IProduct } from '../../models/Iproduct';
+
+@Component({
+  selector: 'app-cart',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css'],
+})
+export class CartComponent implements OnInit {
+  cartProducts: { product: IProduct; quantity: number }[] = [];
+  totalPrice: number = 0;
+
+  constructor(private cartService: CartsService) {}
+
+  ngOnInit(): void {
+    this.cartService.cartProducts.subscribe((products) => {
+      this.cartProducts = products;
+      this.calculateTotalPrice(); // Calculate total price when cart changes
+    });
+  }
+
+  getTotalPrice(product: IProduct, quantity: number): number {
+    return product.price * quantity;
+  }
+
+  calculateTotalPrice(): void {
+    this.totalPrice = this.cartProducts.reduce(
+      (acc, item) => acc + this.getTotalPrice(item.product, item.quantity),
+      0
+    );
+  }
+
+  removeProduct(productId: number): void {
+    this.cartService.removeProductFromCart(productId);
+  }
+
+  increaseQuantity(productId: number): void {
+    this.cartService.increaseQuantity(productId);
+  }
+
+  decreaseQuantity(productId: number): void {
+    this.cartService.decreaseQuantity(productId);
+  }
+}

4eca3315ea6c56d6b7637997e42eb3ba756c0257
ahmedhassan4 <158503741+ahmedhassan4@users.noreply.github.com>
2024-09-09 18:14:31 +0300

    Delete secound-backend/secound-backend directory

--- secound-backend/secound-backend/src/modules/product/product.controller.js
+++
@@ -1,8 +0,0 @@
-import { productModel } from "../../../db/model/product/product.model.js";
-
-let getProducts = async (req, res) => {
-  let products = await productModel.find();
-  res.status(200).json({ products });
-};
-
-export { getProducts };
--- secound-backend/secound-backend/src/modules/product/product.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getProducts } from "./product.controller.js";
-
-let productRoutes = express.Router();
-
-productRoutes.get("/products", getProducts);
-
-export default productRoutes;
